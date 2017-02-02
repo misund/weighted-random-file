611983440afdd4d023b1b353f849d00ff08db858
misund <thomas@hemmeligadresse.com>
2017-02-02 21:47:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+`weighted-random-file` pick a weighted random file, based on it's length.
+
+ The longer the file is, the more often it will be chosen. A 4 kB file will be
+ returned twice as often as a 2 kB file.
+
+ Returns a Promise, so it's thenable.
+
+## Install
+`$ npm install --save weighted-random-file`
+
+## Usage
+```
+var weightedRandomFile = require("weighted-random-file");
+
+weightedRandomFile(['one.txt', 'two.txt', 'three.txt'], __dirname); // "two.txt"
+weightedRandomFile(['one.txt', 'two.txt', 'three.txt'], '/path/to/files'); // "two.txt"
+```
+
+## Parameters
+* an array of relative paths
+* base directory (optional, default is the location of `weighted-random-file`)
+
+## Returns
+A Promise of a string containing a relative path.
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import trailingSlashIt from 'trailing-slash-it';
+
+/**
+ * Pick a weighted random file, based on it's length.
+ *
+ * The longer the file is, the more often it will be chosen. A 4 kB file will be
+ * returned twice as often as a 2 kB file.
+ *
+ * Signature: ([...relativePaths], baseDirectory = __dirname)
+ * Example: weightedRandomFile(['one.txt', 'two.txt', 'three.txt'], __dirname); // "two.txt"
+ * Example: weightedRandomFile(['one.txt', 'two.txt', 'three.txt'], '/path/to/files'); // "two.txt"
+ *
+ * @param an array of relative paths
+ * @param base directory (optional, default is the location of weighted-random-file)
+ * @returns a relative pathname as a Promise of a string
+ */
+const weightedRandomFile = (
+  [...relativePaths],
+  baseDirectory = __dirname,
+) => {
+  const getWeightedFileObject = (path) => {
+    const abspath = trailingSlashIt(baseDirectory) + path;
+
+    return {
+      abspath,
+      path,
+      weight: fs.statSync(abspath).size,
+    };
+  };
+
+  return Promise.resolve(
+    weightedRandomObject(
+      relativePaths.map(getWeightedFileObject)
+    ).path
+  );
+};
